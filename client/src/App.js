05032a1561d876f5534338640bc1d16b9bd57b3c
AnnaW86 <annawebbstewart@gmail.com>
2022-06-23 18:31:33 +0100

    added api

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import CoinContainer from './containers/CoinContainer';
 
 function App() {
   return (
     <>
     <p>Here's the client</p>
+    <CoinContainer />
     </>
   );
 }
---
+++ client/src/containers/CoinContainer.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+const CoinContainer = () => {
+    const [coins, setCoins] = useState(["BTC", "ETH"]);
+    const [coinDataDaily, setCoinDataDaily] = useState([])
+    const [coinData5Min, setCoinData5Min] = useState([])
+
+    const getCoinData5Min = () => {
+        console.log("Getting 5 min coin data");
+        const coinPromises = coins.map((coin) => {
+            return fetch(`https://www.alphavantage.co/query?function=CRYPTO_INTRADAY&symbol=${coin}&market=USD&interval=5min&apikey=7PU7J7PVOJFO2VDL`)
+            .then(response => response.json())})
+        Promise.all(coinPromises)
+        .then((combinedData) => {
+            setCoinData5Min(combinedData);
+            console.log(combinedData);
+        });
+
+    }
+
+    const getCoinDataDaily = () => {
+        console.log("Getting daily coin data");
+        const coinDailyPromises = coins.map((coin) => {
+            return fetch( `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=${coin}&market=CNY&apikey=7PU7J7PVOJFO2VDL`)
+            .then(response => response.json())})
+        Promise.all(coinDailyPromises)
+        .then((combinedData) => {
+            setCoinDataDaily(combinedData);
+            console.log(combinedData);
+        })
+    }
+
+    useEffect(()=> {
+        getCoinData5Min();
+        getCoinDataDaily();
+    }, [])
+
+    return (
+        <>
+            <p>Here's the container</p>
+            
+        </>
+    )
+};
+
+export default CoinContainer
