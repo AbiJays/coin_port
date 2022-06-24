eed2bc4253cd333632713ee0e98612b95e87721a
Abi Johnson <99146064+AbiJays@users.noreply.github.com>
2022-06-24 15:50:46 +0100

    added router page and reorganised

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,13 @@
 import './App.css';
 
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import LogIn from './Pages/LogIn';
-import MyPortfolio from './Pages/MyPortfolio';
-import CoinDetails from './Pages/CoinDetails';
-import NewTransaction from './Pages/NewTransaction';
-import GlobalCurrencies from './Pages/GlobalCurrencies';
-import ErrorPage from './Pages/ErrorPage';
-
 import LogicContainer from './LogicContainer';
 
 function App() {
   return (
     <>
-    {/* <h1>Here's the client</h1> */}
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element ={<LogIn />} />
-        <Route path="/myportfolio" element={<MyPortfolio />} />
-        <Route path="/coin/:slug" element={<CoinDetails />} />
-        <Route path="/newtransaction" element={<NewTransaction />} />
-        <Route path="/global" element={<GlobalCurrencies />} />
-        <Route path="*" element={<ErrorPage />}/>
-      </Routes>
-    </BrowserRouter>
-    {/* <LogicContainer /> */}
+    <h1>Here's the client</h1>
+    
+    <LogicContainer />
     </>
   );
 }
---
+++ client/src/components/Router.js
@@ -0,0 +1,19 @@
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import LogIn from './Pages/LogIn';
+import MyPortfolio from './Pages/MyPortfolio';
+import CoinDetails from './Pages/CoinDetails';
+import NewTransaction from './Pages/NewTransaction';
+import GlobalCurrencies from './Pages/GlobalCurrencies';
+import ErrorPage from './Pages/ErrorPage';
+
+<BrowserRouter>
+      {/* <NavBar /> */}
+      <Routes>
+        <Route exact path="/" element ={<LogIn />} />
+        <Route path="/myportfolio" element={<MyPortfolio />} />
+        <Route path="/coin/:slug" element={<CoinDetails />} />
+        <Route path="/newtransaction" element={<NewTransaction />} />
+        <Route path="/global" element={<GlobalCurrencies />} />
+        <Route path="*" element={<ErrorPage />}/>
+      </Routes>
+    </BrowserRouter>
