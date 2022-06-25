6a4407cad1ee53253d1b842ccff0c1d16353208b
AnnaW86 <annawebbstewart@gmail.com>
2022-06-25 15:28:53 +0100

    added conditional statement to PortfolioCoins

--- client/src/components/Pages/pageComponents/PortfolioCoins.js
+++ client/src/components/Pages/pageComponents/PortfolioCoins.js
@@ -6,27 +6,30 @@ const PortfolioCoins = ({coinDataDaily}) => {
         )
     }
 
+    if (coinDataDaily.length > 0) {
 
-    const coinDataDisplay = coinDataDaily.map(data => {
-        return (
-            <a href={"http://localhost:3000/coin/" + data["Meta Data"]["3. Digital Currency Name"]}>
-                <ul>
-                    <li>{data["Meta Data"]["3. Digital Currency Name"]}</li>
-                    <li>{data["Meta Data"]["2. Digital Currency Code"]}</li>
-                    <li>Average Purchase Price -TBD</li>
-                    <li>Current Global Price - TBD</li>
-                    <li>Investment Value - TBD</li>
-                    <li>Trend Arrow - TBD</li>
-                </ul>
-            </a>
+        const coinDataDisplay = coinDataDaily.map(data => {
+            return (
+                <a href={"http://localhost:3000/coin/" + data["Meta Data"]["3. Digital Currency Name"]}>
+                    <ul>
+                        <li>{data["Meta Data"]["3. Digital Currency Name"]}</li>
+                        <li>{data["Meta Data"]["2. Digital Currency Code"]}</li>
+                        <li>Average Purchase Price -TBD</li>
+                        <li>Current Global Price - TBD</li>
+                        <li>Investment Value - TBD</li>
+                        <li>Trend Arrow - TBD</li>
+                    </ul>
+                </a>
+                )
+            })
+            
+            return (
+                <div>
+                    {coinDataDisplay}
+                </div>
             )
-        })
-        
-        return (
-            <div>
-                {coinDataDisplay}
-            </div>
-        )
+            
+    }
 
 }
 
