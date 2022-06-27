0139c236ac8d6ecb28bd0c4949d850a3b7b0f476
AnnaW86 <annawebbstewart@gmail.com>
2022-06-27 10:31:14 +0100

    added horizontal trend arrow

--- client/src/components/Pages/NewTransaction.js
+++ client/src/components/Pages/NewTransaction.js
@@ -41,18 +41,18 @@ const TransactionForm = (addTransaction) => {
     
     // Copied over from bird sightings
     // Need TransactionService.js file in which to define postTransaction()
-    const [formData, setFormData] = useState({})
-    const onChange = (e) => {
-        formData[e.target.id] = e.target.value;
-        setFormData(formData);
-    }
+    // const [formData, setFormData] = useState({})
+    // const onChange = (e) => {
+    //     formData[e.target.id] = e.target.value;
+    //     setFormData(formData);
+    // }
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        postTransaction(formData).then(() => {
-            addTransaction(formData);
-        })
-    }
+    // const onSubmit = (e) => {
+    //     e.preventDefault();
+    //     postTransaction(formData).then(() => {
+    //         addTransaction(formData);
+    //     })
+    // }
 
 
     
--- client/src/helpers/DisplayHelpers.js
+++ client/src/helpers/DisplayHelpers.js
@@ -1,12 +1,16 @@
 export const getTrend = (trend) => {
-    if (trend >=0) {
+    if (trend > 0) {
         return <span className={["material-symbols-outlined", "trend-up"].join(' ')} >
         trending_up
     </span>
     }
-    else {
+    else if (trend < 0) {
         return <span className={["material-symbols-outlined", "trend-down"].join(' ')} >
         trending_down
+        </span>
+    } else {
+        return <span className={"material-symbols-outlined"} >
+        arrow_right_alt
         </span>
     }
 }
