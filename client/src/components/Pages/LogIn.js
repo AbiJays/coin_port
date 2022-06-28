d58d40c7be69e59f208383be5e1d6be7fd330706
Abi Johnson <99146064+AbiJays@users.noreply.github.com>
2022-06-28 17:04:30 +0100

    class name error

--- client/src/components/Pages/LogIn.js
+++ client/src/components/Pages/LogIn.js
@@ -3,7 +3,7 @@ const LogIn = () => {
     return (
         <>
         <div >
-            <h1 id= "logInHeader">Welcome to <span class="brandName">CoinPort</span></h1>
+            <h1 id= "logInHeader">Welcome to <span className="brandName">CoinPort</span></h1>
             <form id= "logInForm" action="">
                 <label htmlFor="username">Username:</label>
                 <input type="text" id="username" name="username"/>
