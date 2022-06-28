7d714a84dbc52effaf57b2dd88ad06b6c0d3c16d
Abi Johnson <99146064+AbiJays@users.noreply.github.com>
2022-06-28 12:49:42 +0100

    LogIn page and link styled

--- client/src/components/Pages/LogIn.js
+++ client/src/components/Pages/LogIn.js
@@ -2,14 +2,14 @@ const LogIn = () => {
 
     return (
         <>
-        <div>
-            <h1>Welcome to Coin Port</h1>
-            <form action="">
+        <div >
+            <h1 id= "logInHeader">Welcome to <span class="brandName">CoinPort</span></h1>
+            <form id= "logInForm" action="">
                 <label htmlFor="username">Username:</label>
                 <input type="text" id="username" name="username"/>
                 <label htmlFor="password">Password:</label>
                 <input type="password" id="password" name="password"/>
-                <input type="submit" value="Log in"/>
+                <input type="submit" id="logInSubmit" value="Log in"/>
             </form>
 
         </div>
