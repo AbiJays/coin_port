7362b616548f2b840dc41710440d93a4059f0921
AnnaW86 <annawebbstewart@gmail.com>
2022-06-30 00:51:56 +0100

    changing links to coin pages to fix logout bug

--- client/src/components/Pages/pageComponents/GlobalCoinsDisplay.js
+++ client/src/components/Pages/pageComponents/GlobalCoinsDisplay.js
@@ -1,26 +1,31 @@
 import { getTrend, portfolioCoinMarker } from "../../../helpers/DisplayHelpers"
+import { useNavigate } from "react-router-dom"
 
 
 const GlobalCoinsDisplay = ({liveCoinData, portfolioData}) => {
+    let navigate = useNavigate();
 
     if (liveCoinData.length > 0) {
         const globalCoinDataDisplay = liveCoinData.map(coin => {
-            
+            function goToCoin() {
+                console.log(coin.abbreviation)
+                navigate(`/coin/${coin.abbreviation}`)
+              }
             return (
                 <>
                     <tr className="table-row">
-                        <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{portfolioCoinMarker(portfolioData, coin.abbreviation)}</a></td>
-                        <td id="globalCoinName"><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{coin.name}</a></td>
-                        <td>
-                            <a href={"http://localhost:3000/coin/" + coin.abbreviation}>
+                        <td align="center" className="centered-cell"><div onClick={goToCoin}>{portfolioCoinMarker(portfolioData, coin.abbreviation)}</div></td>
+                        <td id="globalCoinName"><div onClick={goToCoin}>{coin.name}</div></td>
+                        <td align="center" className="centered-cell">
+                        <div onClick={goToCoin}>
                             <div>
                                 <img src={coin.logo} alt={coin.abbreviation + "logo"} className="logo" />
                                 <p>{coin.abbreviation}</p> 
                             </div>
-                            </a>
+                            </div>
                         </td>
-                        <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{parseFloat(coin.price).toFixed(2)}</a></td>
-                        <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{getTrend(coin['1d'])}</a></td>
+                        <td align="center" className="centered-cell"><div onClick={goToCoin}>{parseFloat(coin.price).toFixed(2)}</div></td>
+                        <td align="center" className="centered-cell"><div onClick={goToCoin}>{getTrend(coin['1d'])}</div></td>
                     </tr>
                         {/* <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}><img src={coin.logo} alt={coin.abbreviation + "logo"} className="logo" /></a></td> */}
                 </>
--- client/src/components/Pages/pageComponents/SingleCoinDisplay.js
+++ client/src/components/Pages/pageComponents/SingleCoinDisplay.js
@@ -1,24 +1,33 @@
 import { getTrend } from "../../../helpers/DisplayHelpers";
+import { useNavigate } from "react-router-dom";
 
 const SingleCoinDisplay = ({coin}) => {
+    let navigate = useNavigate();
 
+    function goToCoin() {
+        console.log(coin.abbreviation)
+        navigate(`/coin/${coin.abbreviation}`)
+      }
+    // return <button onClick={goToCoin}>button</button>
     return (
         <>
+
+
         <tr key={coin.abbreviation} className="table-row" >
-            {/* <td rowSpan="2"><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{coin.name}</a></td> */}
-            <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{coin.name}</a></td>
-            <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>
+            <td><div onClick={goToCoin}>{coin.name}</div></td>
+            {/* <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{coin.name}</a></td> */}
+            <td><div onClick={goToCoin}>
                 <div>
                     <img src={coin.logo} alt={coin.abbreviation + "logo"} className="logo" />
                     <p>{coin.abbreviation}</p>
                 </div>
-                </a>
+                </div>
             </td>
-            <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{coin.portfolioQuantity}</a></td>
-            <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{parseFloat(coin.currentPrice).toFixed(3)}</a></td>
-            <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{coin.investmentValue}</a></td>
-            <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{getTrend(coin.trend)}</a></td>
-            <td><a href={"http://localhost:3000/coin/" + coin.abbreviation}>{coin.profitAndLoss}</a></td>
+            <td align="center" className="centered-cell"><div onClick={goToCoin} >{coin.portfolioQuantity}</div></td>
+            <td align="center" className="centered-cell"><div onClick={goToCoin}>{parseFloat(coin.currentPrice).toFixed(3)}</div></td>
+            <td align="center" className="centered-cell"><div onClick={goToCoin}>{coin.investmentValue}</div></td>
+            <td align="center" className="centered-cell"><div onClick={goToCoin}>{getTrend(coin.trend)}</div></td>
+            <td align="center" className="centered-cell"><div onClick={goToCoin}>{coin.profitAndLoss}</div></td>
         </tr>
         </>
 )}
